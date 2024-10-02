fc089b806bc394955ea0919956c2a3819c356118
mynkduh <mynkdas@gmail.com>
2024-10-02 20:06:06 +0530

    package name changes

--- src/main/java/com/jwtauth/jwtsecurity/jwtsecurityApplication.java
+++ src/main/java/com/jwtauth/jwtsecurity/jwtsecurityApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class jwtsecurity {
+public class jwtsecurityApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(jwtsecurity.class, args);
+        SpringApplication.run(jwtsecurityApplication.class, args);
     }
 }
